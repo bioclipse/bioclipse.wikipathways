eb4467ac0ea355528c2dc81486ba43b9fa6af110
Egon Willighagen <egon.willighagen@gmail.com>
2013-08-11 22:18:30 +0200

    Commented out methods that are not implemented yet

--- plugins/net.bioclipse.wikipathways/src/net/bioclipse/wikipathways/business/IWikipathwaysManager.java
+++ plugins/net.bioclipse.wikipathways/src/net/bioclipse/wikipathways/business/IWikipathwaysManager.java
@@ -47,75 +47,75 @@ public interface IWikipathwaysManager extends IBioclipseManager {
     public String listPathways(String organism)
 	throws ParserConfigurationException, SAXException, IOException, TransformerException;
 	  
-	  @Recorded
-	  @PublishedMethod(
-	        params = "String pwId, Integer revision", 
-	        methodSummary = "Download the pathway from WikiPathways."
-	        	
-	    )
-    public String getPathways(String pwId, Integer revision)
-	throws ParserConfigurationException, SAXException, IOException, TransformerException;
-	  
-	  @Recorded
-	  @PublishedMethod(
-	        params = "String pwId", 
-	        methodSummary = "Get some general info about the pathway, such as the name, species, without downloading the GPML."
-	        	
-	    )
-    public String getPathwayInfo(String pwId)
-	throws ParserConfigurationException, SAXException, IOException, TransformerException;
-	  
-	  @Recorded
-	  @PublishedMethod(
-	        params = "String pwId, String timestamp", 
-	        methodSummary = "Get the revision history of a pathway."
-	        	
-	    )
-    public String getPathwayHistory(String pwId)
-	throws ParserConfigurationException, SAXException, IOException, TransformerException;
-	  
-	  @Recorded
-	  @PublishedMethod(
-	        params = "String timestamp", 
-	        methodSummary = "Get the recently changed pathways."
-	        	
-	    )
-    public String getRecentChanges(String timestamp)
-	throws ParserConfigurationException, SAXException, IOException, TransformerException;
-	  
-	  @Recorded
-	  @PublishedMethod(
-	        params = "Find pathways using a textual search on the description and text labels of the pathway objects.", 
-	        methodSummary = "Get the recently changed pathways."
-	        	
-	    )
-    public String findPathwaysByText(String query, String species)
-	throws ParserConfigurationException, SAXException, IOException, TransformerException;
-	  
-	  @Recorded
-	  @PublishedMethod(
-	        params = "Find pathways using a textual search on the description and text labels of the pathway objects.", 
-	        methodSummary = "Get the recently changed pathways."
-	        	
-	    )
-    public String findPathwaysByXref(String[] ids, String codes)
-	throws ParserConfigurationException, SAXException, IOException, TransformerException;
-	  
-	  @Recorded
-	  @PublishedMethod(
-	        params = "Find pathways using a textual search on the description and text labels of the pathway objects.", 
-	        methodSummary = "Get the recently changed pathways."
-	        	
-	    )
-    public String findInteractions(String query)
-	throws ParserConfigurationException, SAXException, IOException, TransformerException;
-	  
-	  @Recorded
-	  @PublishedMethod(
-	        params = "Find pathways using a textual search on the description and text labels of the pathway objects.", 
-	        methodSummary = "Get the recently changed pathways."
-	        	
-	    )
-    public String findPathwaysByLiterature(String query)
-	throws ParserConfigurationException, SAXException, IOException, TransformerException;
+//	  @Recorded
+//	  @PublishedMethod(
+//	        params = "String pwId, Integer revision", 
+//	        methodSummary = "Download the pathway from WikiPathways."
+//	        	
+//	    )
+//    public String getPathways(String pwId, Integer revision)
+//	throws ParserConfigurationException, SAXException, IOException, TransformerException;
+//	  
+//	  @Recorded
+//	  @PublishedMethod(
+//	        params = "String pwId", 
+//	        methodSummary = "Get some general info about the pathway, such as the name, species, without downloading the GPML."
+//	        	
+//	    )
+//    public String getPathwayInfo(String pwId)
+//	throws ParserConfigurationException, SAXException, IOException, TransformerException;
+//	  
+//	  @Recorded
+//	  @PublishedMethod(
+//	        params = "String pwId, String timestamp", 
+//	        methodSummary = "Get the revision history of a pathway."
+//	        	
+//	    )
+//    public String getPathwayHistory(String pwId)
+//	throws ParserConfigurationException, SAXException, IOException, TransformerException;
+//	  
+//	  @Recorded
+//	  @PublishedMethod(
+//	        params = "String timestamp", 
+//	        methodSummary = "Get the recently changed pathways."
+//	        	
+//	    )
+//    public String getRecentChanges(String timestamp)
+//	throws ParserConfigurationException, SAXException, IOException, TransformerException;
+//	  
+//	  @Recorded
+//	  @PublishedMethod(
+//	        params = "Find pathways using a textual search on the description and text labels of the pathway objects.", 
+//	        methodSummary = "Get the recently changed pathways."
+//	        	
+//	    )
+//    public String findPathwaysByText(String query, String species)
+//	throws ParserConfigurationException, SAXException, IOException, TransformerException;
+//	  
+//	  @Recorded
+//	  @PublishedMethod(
+//	        params = "Find pathways using a textual search on the description and text labels of the pathway objects.", 
+//	        methodSummary = "Get the recently changed pathways."
+//	        	
+//	    )
+//    public String findPathwaysByXref(String[] ids, String codes)
+//	throws ParserConfigurationException, SAXException, IOException, TransformerException;
+//	  
+//	  @Recorded
+//	  @PublishedMethod(
+//	        params = "Find pathways using a textual search on the description and text labels of the pathway objects.", 
+//	        methodSummary = "Get the recently changed pathways."
+//	        	
+//	    )
+//    public String findInteractions(String query)
+//	throws ParserConfigurationException, SAXException, IOException, TransformerException;
+//	  
+//	  @Recorded
+//	  @PublishedMethod(
+//	        params = "Find pathways using a textual search on the description and text labels of the pathway objects.", 
+//	        methodSummary = "Get the recently changed pathways."
+//	        	
+//	    )
+//    public String findPathwaysByLiterature(String query)
+//	throws ParserConfigurationException, SAXException, IOException, TransformerException;
 }
